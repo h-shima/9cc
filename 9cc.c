baf874a915e8971511178757c00e579edab01816
Hiroki Oshima <geet1876@gmail.com>
2020-04-29 18:01:59 +0900

    加算、減算をASTツリーを使用してコンパイルできるように更新

--- 9cc.c
+++ 9cc.c
@@ -12,9 +12,9 @@ typedef enum {
 	TK_EOF, // 入力の終わりを表すトークン
 } TokenKind;
 
+// トークン型
 typedef struct Token Token;
 
-// トークン型
 struct Token {
 	TokenKind kind; // トークンの型
 	Token *next; // 次の入力のトークン
@@ -93,8 +93,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 }
 
 // Tokenize `user_input` and returns new tokens.
-Token *tokenize() {
-	char *p = user_input;
+Token *tokenize(char *input) {
+	char *p = input;
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
@@ -123,37 +123,115 @@ Token *tokenize() {
 	return head.next;
 }
 
+// 抽象構文木のノードの種類
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // 整数
+} NodeKind;
+
+// 抽象構文木のノードの型
+typedef struct Node Node;
+
+struct Node {
+	NodeKind kind; // ノードの型
+	Node *lhs;     // 左辺 (left-hand side)
+	Node *rhs;     // 右辺 (right-hand side)
+	int val;       // kind が ND_NUM の場合のみ使う
+};
+
+// プロトタイプ宣言
+Node *expr();
+Node *mul();
+Node *primary();
+void gen(Node *node);
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs  = lhs;
+	node->rhs  = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val  = val;
+	return node;
+}
+
+Node *expr() {
+	Node *node = mul();
+
+	for (;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+// TODO: 後から*, /, ()に対応
+// 今は数字のノードを返すだけ
+Node *mul() {
+	Node *node = primary();
+	return node;
+}
+
+// TODO: 後から '(' expr ')' に対応
+Node *primary() {
+	return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("	push %d\n", node->val);
+		return;
+	}
+
+	// 現ノードの左側をコンパイル
+	gen(node->lhs);
+	// 現ノードの右側をコンパイル
+	gen(node->rhs);
+
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch (node->kind) {
+		case ND_ADD:
+			printf("	add rax, rdi\n");
+			break;
+		case ND_SUB:
+			printf("	sub rax, rdi\n");
+			break;
+	}
+
+	// 計算結果をraxレジスタにプッシュする
+	printf("	push rax\n");
+}
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		error("引数の個数が正しくありません");
 		return 1;
 	}
 
-	// 引数で渡された文字列全体をuser_inputに保存する
+	// 引数で渡された文字列全体をトークナイズしてパースする
 	user_input = argv[1];
-	// user_inputをトークナイズする
-	token = tokenize();
+	token = tokenize(user_input);
+	Node *node = expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	// 式の最初は数でなければならないので、それをチェックして
-	// 最初のmov命令を出力
-	printf("	mov rax, %d\n", expect_number());
+	gen(node);
 
-	// `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-	// アセンブリを出力
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("	add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("	sub rax, %d\n", expect_number());
-	}
-
+	printf("	pop rax\n");
 	printf("	ret\n");
-	return 0;
 }
